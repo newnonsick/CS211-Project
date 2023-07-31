016eb73d74238bd2cc36cc22ef294bd227148d42
Tarn <jirayu.o@ku.th>
2023-07-31 20:41:04 +0700

    make mainpage be alble to switch between scenes

--- src/main/java/cs211/project/controllers/MainPageController.java
+++ src/main/java/cs211/project/controllers/MainPageController.java
@@ -1,4 +1,30 @@
 package cs211.project.controllers;
 
+import cs211.project.services.FXRouter;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
 public class MainPageController {
+    @FXML
+    BorderPane window;
+    @FXML
+    StackPane content;
+    @FXML
+    public void goToTestPage1() throws IOException {
+        Parent fxml = FXMLLoader.load(getClass().getResource("/cs211/project/views/testpage1.fxml"));
+        content.getChildren().removeAll();
+        content.getChildren().setAll(fxml);
+    }
+
+    @FXML
+    public void goToTestPage2() throws IOException {
+        Parent fxml = FXMLLoader.load(getClass().getResource("/cs211/project/views/testpage2.fxml"));
+        content.getChildren().removeAll();
+        content.getChildren().setAll(fxml);
+    }
 }
