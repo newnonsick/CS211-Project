7987235ae1f54955bfedf360a2c06aea9b3f241f
Tarn <jirayu.o@ku.th>
2023-08-09 20:43:30 +0700

    Make the event list page with components, Fix the routing from previous commits.

---
+++ src/main/java/cs211/project/controllers/EventListController.java
@@ -0,0 +1,15 @@
+package cs211.project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class EventListController {
+    @FXML
+    ListView eventListView;
+    @FXML
+    TextField searchTextField;
+    @FXML
+    Button searchButton;
+}
