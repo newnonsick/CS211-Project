e1769280f52a2f0c1e2c0ab796c91eb135db1613
newnonsick <thitivath.m@ku.th>
2023-08-11 03:22:22 +0700

    Add Autocomplete in Search Event List

--- src/main/java/cs211/project/controllers/EventListController.java
+++ src/main/java/cs211/project/controllers/EventListController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class EventListController {
     @FXML
@@ -13,8 +15,35 @@ public class EventListController {
     @FXML
     Button searchButton;
 
-    @FXML
-    public void initialize(){
-        ;
+    public void initialize() {
+        searchTextField.setOnKeyReleased(this::handleAutoComplete);
     }
+
+    private void handleAutoComplete(KeyEvent event) {
+        String enteredText = searchTextField.getText();
+        if (event.getCode() == KeyCode.BACK_SPACE || enteredText.isEmpty()) {
+            return;
+        }
+
+        String matchedSuggestion = null;
+        int totalMatched = 0;
+        for (String suggestion : SUGGESTIONS_ARRAY) {
+            if (suggestion.toLowerCase().startsWith(enteredText.toLowerCase())) {
+                matchedSuggestion = suggestion;
+                totalMatched++;
+                if (totalMatched > 1) {
+                    break;
+                }
+            }
+        }
+
+        if (totalMatched == 1 && matchedSuggestion != null && !matchedSuggestion.equals(enteredText)) {
+            searchTextField.setText(matchedSuggestion);
+            searchTextField.selectRange(enteredText.length(), matchedSuggestion.length());
+        }
+    }
+
+    private static final String[] SUGGESTIONS_ARRAY = {
+            "New", "Tarn", "Ice", "Nutt"
+    };
 }
