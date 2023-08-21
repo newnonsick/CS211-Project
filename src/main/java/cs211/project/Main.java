eddbc0b5eaf56023c8de8be83576f7e5538b311b
Tarn Jirayu <jirayu.o@ku.th>
2023-08-21 16:02:37 +0700

    Fixing typo in main

--- src/main/java/cs211/project/Main.java
+++ src/main/java/cs211/project/Main.java
@@ -1,7 +1,7 @@
 package cs211.project;
 import cs211.project.cs211661project.MainApplication;
 
-public class cd Main {
+public class Main {
     public static void main(String[] args) {
         MainApplication.main(args);
     }
