719393e83225833657954c50d5e70f478809c26a
Pimmada <pimmada.pan@ku.th>
2023-08-29 20:20:34 +0700

    Delete Participant

--- src/main/java/cs211/project/controllers/EventParticipantManagementController.java
+++ src/main/java/cs211/project/controllers/EventParticipantManagementController.java
@@ -1,9 +1,6 @@
 package cs211.project.controllers;
 
-import cs211.project.models.Participant;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
