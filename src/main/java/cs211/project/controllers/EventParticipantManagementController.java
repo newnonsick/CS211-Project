82225480154db56068be9838df8cadc0c5e10998
preangkwan <preangkwan.c@ku.th>
2023-08-30 22:35:13 +0700

    Edit fxml and controller of participants

--- src/main/java/cs211/project/controllers/EventParticipantManagementController.java
+++ src/main/java/cs211/project/controllers/EventParticipantManagementController.java
@@ -1,11 +1,9 @@
 package cs211.project.controllers;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import cs211.project.services.FXRouterPane;
 import javafx.scene.control.cell.PropertyValueFactory;
 
@@ -15,6 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventParticipantManagementController {
+    @FXML private TableView eventParticipantTableView;
+    @FXML private Button goToEditParticipant;
+    @FXML private Label eventName;
+    @FXML private TableView activityParticipantTableView;
+    @FXML private TextField avtivityNameTextField;
+    @FXML private TextField activityPartiDetailsTextField;
+
 
 
     @FXML
@@ -32,5 +37,14 @@ public class EventParticipantManagementController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void handleEndActivityPartiButton(ActionEvent actionEvent) {
+    }
+
+    public void handleAddActivityPartiButton(ActionEvent actionEvent) {
+    }
+
+    public void handleRemoveActivityPartiButton(ActionEvent actionEvent) {
     }
 }
