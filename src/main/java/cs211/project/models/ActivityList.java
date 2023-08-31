624fbacf0ab2808aeba6fa2c9591415eff5cfc29
newnonsick <thitivath.m@ku.th>
2023-09-01 01:30:45 +0700

    Update Autocomplete add add method addNewActivity, findActivityByName in ActivityList

--- src/main/java/cs211/project/models/ActivityList.java
+++ src/main/java/cs211/project/models/ActivityList.java
@@ -12,4 +12,24 @@ public class ActivityList {
     public ArrayList<Activity> getActivities() {
         return activities;
     }
+
+    public void addNewActivity(String activityName, String activityInformation) {
+        activityName = activityName.trim();
+        activityInformation = activityInformation.trim();
+        if (!activityName.equals("") && !activityInformation.equals("")) {
+            Activity exist = findActivityByName(activityName);
+            if (exist == null) {
+                activities.add(new Activity(activityName, activityInformation));
+            }
+        }
+    }
+
+    public Activity findActivityByName(String activityName) {
+        for (Activity activity : activities) {
+            if (activity.getActivityName().equals(activityName)) {
+                return activity;
+            }
+        }
+        return null;
+    }
 }
