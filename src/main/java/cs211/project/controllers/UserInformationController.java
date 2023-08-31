68ff2a1b4db2e908fec6fd454668269f90aea932
Pimmada <pimmada.pan@ku.th>
2023-08-31 18:58:35 +0700

    Add showUser in UserinformationController

--- src/main/java/cs211/project/controllers/UserInformationController.java
+++ src/main/java/cs211/project/controllers/UserInformationController.java
@@ -1,9 +1,15 @@
 package cs211.project.controllers;
 
+import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import cs211.project.models.User;
+import cs211.project.services.CurrentUser;
 
 public class UserInformationController {
     @FXML private Label usernameLabel;
@@ -11,4 +17,33 @@ public class UserInformationController {
     @FXML private TableView activeEventTableView;
     @FXML private TableView eventHistoryTableview;
     @FXML private ImageView profileImageView;
+
+    private CurrentUser currentUser;
+
+    public void initialize() {
+        checkFileIsExisted("userData.csv");
+    }
+
+    private void checkFileIsExisted(String fileName) {
+        File file = new File("data");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        String filePath = "data" + File.separator + fileName;
+        file = new File(filePath);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void showUser(CurrentUser currentUser) {
+        User user = currentUser.getUser();
+        nameLabel.setText(user.getName());
+        usernameLabel.setText(user.getUsername());
+        // profile picture
+    }
 }
