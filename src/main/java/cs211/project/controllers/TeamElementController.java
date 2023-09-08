b33613b198f5d5adeada61c797bd7ceb7b5c604b
newnonsick <thitivath.m@ku.th>
2023-09-09 02:14:36 +0700

    add show team of event

---
+++ src/main/java/cs211/project/controllers/TeamElementController.java
@@ -0,0 +1,30 @@
+package cs211.project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+
+import java.io.File;
+import java.time.LocalDate;
+
+public class TeamElementController {
+    @FXML
+    Label eventNameLabel;
+    @FXML
+    Label teamNameLabel;
+    @FXML
+    Label maxParticipantsLabel;
+    @FXML
+    Label startJoinDateLabel;
+    @FXML
+    Label closingJoinDateLabel;
+
+    public void setPage(String eventName, String teamName, int maxParticipants, LocalDate startJoinDate, LocalDate closingJoinDate) {
+        eventNameLabel.setText(eventName);
+        teamNameLabel.setText(teamName);
+        maxParticipantsLabel.setText(String.valueOf(maxParticipants));
+        startJoinDateLabel.setText("" + startJoinDate);
+        closingJoinDateLabel.setText("" + closingJoinDate);
+    }
+
+}
--- src/main/java/cs211/project/controllers/TeamOfEventListController.java
+++ src/main/java/cs211/project/controllers/TeamOfEventListController.java
@@ -1,73 +1,95 @@
 package cs211.project.controllers;
 
+import cs211.project.models.Event;
 import cs211.project.models.Team;
 import cs211.project.models.TeamList;
 import cs211.project.services.Datasource;
 import cs211.project.services.FXRouterPane;
 import cs211.project.services.TeamListFileDatasource;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 public class TeamOfEventListController {
-    @FXML Label teamNameLabel;
-    @FXML Label numPeopleLabel;
-    @FXML Label startDateLabel;
-    @FXML Label endDateLabel;
-    @FXML TableView teamListTableView;
+
+    @FXML
+    ScrollPane teamScrollPane;
+    @FXML
+    GridPane teamGridPane;
+    @FXML
+    Label eventNameLabel;
 
     private Datasource<TeamList> datasource;
     private TeamList teamList;
     private String eventName;
+    private LocalDate currentDate;
 
     @FXML
     public void initialize(){
         eventName = (String) FXRouterPane.getData();
+        eventNameLabel.setText(eventName);
+        ZoneId thaiTimeZone = ZoneId.of("Asia/Bangkok");
+        currentDate = LocalDate.now(thaiTimeZone);
         datasource = new TeamListFileDatasource("data", "team_list.csv");
         teamList = datasource.readData();
-        showTeam(teamList);
+        //showTeam(teamList);
+        showTeam();
     }
 
-    public void handleJoinTeamButton(){
-        ;
-    }
 
-    private void showTeam(TeamList teamList){
-        TableColumn<Team, String> eventNameColum = new TableColumn<>("Event Name");
-        eventNameColum.setCellValueFactory(new PropertyValueFactory<>("eventOfTeamName"));
-
-        TableColumn<Team, String> teamNameColum = new TableColumn<>("Team Name");
-        teamNameColum.setCellValueFactory(new PropertyValueFactory<>("teamName"));
-
-        TableColumn<Team, Integer> maxParticipantsColumn = new TableColumn<>("Max Participants");
-        maxParticipantsColumn.setCellValueFactory(new PropertyValueFactory<>("maxParticipants"));
-
-        TableColumn<Team, LocalDate> startJoinDateColumn = new TableColumn<>("Start Join Date");
-        startJoinDateColumn.setCellValueFactory(new PropertyValueFactory<>("startJoinDate"));
-
-        TableColumn<Team, LocalDate> closingJoinDateColumn = new TableColumn<>("End Join Date");
-        closingJoinDateColumn.setCellValueFactory(new PropertyValueFactory<>("closingJoinDate"));
-
-
-        teamListTableView.getColumns().clear();
-        teamListTableView.getColumns().add(eventNameColum);
-        teamListTableView.getColumns().add(teamNameColum);
-        teamListTableView.getColumns().add(maxParticipantsColumn);
-        teamListTableView.getColumns().add(startJoinDateColumn);
-        teamListTableView.getColumns().add(closingJoinDateColumn);
-
-        teamListTableView.getItems().clear();
-
+    public void showTeam(){
+        int row = 0;
+        int column = 0;
         for (Team team : teamList.getTeams()) {
-            if (team.getEventOfTeamName().equals(eventName)) {
-                teamListTableView.getItems().add(team);
+            if (!team.getEventOfTeamName().equals(eventName)) {
+                continue;
             }
+            long daysBetween = ChronoUnit.DAYS.between(currentDate, team.getClosingJoinDate());
+            if (daysBetween < 0) {
+                continue;
+            }
+            if(column == 2) {
+                row++;
+                column = 0;
+            }
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/cs211/project/views/team-element.fxml"));
+            AnchorPane anchorPane = null;
+            try {
+                anchorPane = fxmlLoader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            TeamElementController team_ = fxmlLoader.getController();
+            team_.setPage(team.getEventOfTeamName(), team.getTeamName(), team.getMaxParticipants(), team.getStartJoinDate(), team.getClosingJoinDate());
+            anchorPane.setOnMouseClicked(event1 -> {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setHeaderText("Do you want to join this team?");
+                Optional<ButtonType> result = alert.showAndWait();
+
+                if(result.isEmpty()){
+                    System.out.println("Alert closed");
+                } else if(result.get() == ButtonType.OK){
+                    System.out.println("OK!");
+                } else if(result.get() == ButtonType.CANCEL){
+                    System.out.println("Never!");
+                }
+            });
+            teamGridPane.add(anchorPane, column, row);
+            column++;
+
+            GridPane.setMargin(anchorPane, new Insets(10));
         }
-
-
     }
 }
