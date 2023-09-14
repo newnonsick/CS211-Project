8264697aca7f47f32058da40da76e915da48b262
Tarn Jirayu <jirayu.o@ku.th>
2023-09-14 14:52:43 +0700

    Admin log
    add admin log and show the picture of the user

--- src/main/java/cs211/project/models/LogUser.java
+++ src/main/java/cs211/project/models/LogUser.java
@@ -2,12 +2,16 @@ package cs211.project.models;
 
 public class LogUser extends User {
     private String logTime;
-    public LogUser(String username, String logTime) {
+    private String logDate;
+    public LogUser(String username, String logDate, String logTime) {
         super(username);
         this.logTime = logTime;
+        this.logDate = logDate;
     }
 
     public String getLogTime() {
         return this.logTime;
     }
+
+    public String getLogDate() { return this.logDate;}
 }
